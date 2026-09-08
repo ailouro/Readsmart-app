91c4574bba50a90afb8437dd255b6b94749e33ff
Lourene <lourenegutierrez2@gmail.com>
2026-09-08 17:54:38 +0800

    Remove const causing web build errors, pin Flutter version

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'screens/login_screen.dart';
+import 'package:flutter/cupertino.dart';
 
 void main() async {
   // Must be called before using platform channels or plugins
